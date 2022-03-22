681a5b55eaa407b3dd4d5baceb283ae9d5680c15
sebastian.dev <sebastian.villalob@gmail.com>
2022-03-22 20:12:13 -0300

    Añadiendo contacto

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
+
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import './Footer.scss';
 
 const Footer = () => {
+  const form = useRef();
+  const [finished, setFinished] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
+
   return (
     <div>Footer</div>
   )
